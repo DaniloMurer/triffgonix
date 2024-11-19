92a09563a58cd651cee80cf64b62b54d93c944b2
Danilo Jakob <danilojakob@protonmail.com>
2024-11-19 21:15:57 +0100

    feat(server): missing changes

--- server/internal/triffgonix/database/database_test.go
+++ server/internal/triffgonix/database/database_test.go
@@ -1,35 +1,35 @@
 package database
 
 import (
-  "github.com/google/uuid"
-  "server/internal/triffgonix/models"
-  "testing"
+	"github.com/google/uuid"
+	"server/internal/triffgonix/models"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAutoMigrate(t *testing.T) {
-  assert.NotPanics(t, AutoMigrate)
+	assert.NotPanics(t, AutoMigrate)
 }
 
 func TestCreatePlayer(t *testing.T) {
-  player := &models.Player{PlayerName: uuid.Must(uuid.NewRandom()).String()}
-  err, _ := CreatePlayer(player)
-  assert.Nil(t, err)
+	player := &models.Player{PlayerName: uuid.Must(uuid.NewRandom()).String()}
+	err, _ := CreatePlayer(player)
+	assert.Nil(t, err)
 }
 
 func TestFindAllUsers(t *testing.T) {
-  users := FindAllUsers()
-  assert.NotNil(t, users)
+	users := FindAllUsers()
+	assert.NotNil(t, users)
 }
 
 func TestCreateGame(t *testing.T) {
-  game := &models.Game{Name: "Test Game"}
-  err, _ := CreateGame(game)
-  assert.Nil(t, err)
+	game := &models.Game{Name: "Test Game"}
+	err, _ := CreateGame(game)
+	assert.Nil(t, err)
 }
 
 func TestFindAllGames(t *testing.T) {
-  games := FindAllGames()
-  assert.NotNil(t, games)
+	games := FindAllGames()
+	assert.NotNil(t, games)
 }
